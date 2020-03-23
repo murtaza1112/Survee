94a752bbcb24a6bb45baa781d4323c1501d4e134
murtaza1112 <54723469+murtaza1112@users.noreply.github.com>
2020-03-23 21:30:28 +0530

    r10 error

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
 app.listen(PORT, () => {
   console.log("The server is active on:", PORT);
 });
