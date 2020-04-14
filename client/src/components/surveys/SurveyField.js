f8e1a8c4f9a2f769fec5cc84a9ac645b81270721
murtaza1112 <54723469+murtaza1112@users.noreply.github.com>
2020-04-14 16:59:00 +0530

    removd comment

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,13 +2,10 @@ import React from "react";
 
 class SurveyField extends React.Component {
   render() {
-    //reducfrom provides this value
     const input = this.props.input;
     const meta = this.props.meta;
     const { error, touched } = meta;
-    // console.log(meta);
-    // get touched value as the form render in the beginnign as well so renders an error Before
-    // user even interacts with the form
+
     return (
       <div>
         <label>{this.props.label}</label>
